14ff70d1c2efe4e19fcb6de392ad37bb40cabbf1
vedantA04 <vedantagarwal2004@gmail.com>
2023-08-12 19:44:42 +0530

    finale for presentation

--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,8 @@
 f = open("test-pyramid.gcode", "w")
-init_X = 180
-fin_X = 150
-init_Y = 90
-fin_Y = 130
+init_X = 220
+fin_X = 170
+init_Y = 100
+fin_Y = 150
 init_Z = 96.5
 # f.write("G28 X Y Z\n")
 # f.write("G0 Z98\n")
@@ -11,38 +11,43 @@ for i in range(1, 100):
         f.write("; layer" + str(i+1) + "\n")
         f.write("G0 Z" + str(init_Z) + "\n")
         f.write("G92 E0\n")
-        f.write("G1 E3 F150\n")
-        f.write("G0 X315 F500\n")
-        f.write("G0 Z" + str(init_Z-2) + "\n")
+        f.write("G1 E3.5 F150\n")
+        f.write("G0 X315 F1000\n")
+        f.write("G0 Z" + str(init_Z-5) + "\n")
+        f.write("G1 E-3.5 F150\n")
         temp_x = init_X
         for j in range(1, 100):#code for the square layer
+            temp_speed = 8000
+            if j == 1:
+                temp_speed = 2000
             if temp_x - 5 >= fin_X:
-                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F2100\n")
-                f.write("M106 S200; pump and valve on\n")
-                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F2100\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F" + str(temp_speed) + "\n")
+                f.write("M106 S230; pump and valve on\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F8000\n")
                 temp_x = temp_x - 5
-                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F2100\n")
-                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F2100\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F8000\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F8000\n")
                 temp_x = temp_x - 5
             elif temp_x>=fin_X:
-                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F2100\n")
-                f.write("M106 S200; pump and valve on\n")
-                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F2100\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(init_Y) + " F" + str(temp_speed) + "\n")
+                f.write("M106 S230; pump and valve on\n")
+                f.write("G0 X" + str(temp_x) + " Y" + str(fin_Y) + " F8000\n")
                 temp_x = temp_x - 5
             else:
                 break
         f.write("M107\n")
         f.write("G0 X0 Y0 F2000\n")
+        f.write("G1 E3 F150\n")
         init_X = init_X - 2.5
         fin_X = fin_X + 2.5
         init_Y = init_Y + 2.5
         fin_Y = fin_Y - 2.5
-        init_Z = init_Z - 2
+        init_Z = init_Z - 3
 
 f.write("; final layer\n")
 f.write("G0 Z" + str(init_Z) + "\n")
 f.write("G92 E0\n")
-f.write("G1 E3 F150\n")
+f.write("G1 E3.5 F150\n")
 f.write("G0 X315 F500\n")
 
 f.close()
--- trial3.gcode
+++ trial3.gcode
@@ -1,6 +1,6 @@
 G28 Y Z
-G0 Z92
-G0 Y80 F2000;extruder placement
+G0 Z95
+G0 Y90 F2000;extruder placement
 M106 S200; pump and valve on
 G0 Y170 F2400;extruder placement
 M107; pump and valve off
